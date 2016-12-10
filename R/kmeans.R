e90ea32dc9b40a3a4da4f7a0a5c5bfce396c5436
baba-beda <darya.nightmare@gmail.com>
2016-12-10 23:13:57 +0300

    eliminated problem with rows full of NAs

--- R/kmeans.R
+++ R/kmeans.R
@@ -12,6 +12,9 @@ kmeans <- function(es, cols = c(), rows = c(), k, replacena = "mean") {
   if (nrow(ind) > 0) {
     data[ind] <- apply(data, 1, replacena, na.rm = T)[ind[,1]]
   }
+  ind1 <- which(!is.nan(as.matrix(data)), arr.ind = T)
+  left.rows <- unique(ind1[,"row"])
+  data <- data[left.rows,]
   data <- t(scale(t(data)))
   km <- stats::kmeans(data, k)
   res <- data.frame(row.names = row.names(exprs(es)))
