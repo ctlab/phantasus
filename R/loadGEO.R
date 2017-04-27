a13a1ddc99bd0cd91544b12caa045db7397a9665
baba-beda <darya.nightmare@gmail.com>
2017-04-27 16:43:43 +0300

    extracted featureData editing to getGSE

--- R/loadGEO.R
+++ R/loadGEO.R
@@ -16,16 +16,66 @@ loadGEO <- function(name, type = NA) {
   participants <- colnames(es)
   rownames <- rownames(es)
 
-  fdata <- as.matrix(fData(es)[,grepl("symbol", varLabels(featureData(es)), ignore.case = T)])
+  fdata <- as.matrix(fData(es))
   colnames(fdata) <- NULL
   row.names(fdata) <- NULL
 
-  varlabels <- if (ncol(fdata) > 0) "symbol" else NULL
-  fdata <- if (is.null(varlabels)) NULL else fdata[,1]
-
-  res <- list(data = data, pdata = pdata, participants = participants, symbol = fdata, rownames = rownames, colMetaNames = colnames(pData(es.loaded)))
+  res <- list(data = data, pdata = pdata, participants = participants, symbol = fdata, rownames = rownames, colMetaNames = varLabels(phenoData(es)), rowMetaNames = varLabels(featureData(es)))
 
   f <- tempfile(pattern = "gse", tmpdir = getwd(), fileext = ".bin")
   writeBin(protolite::serialize_pb(res), f)
   f
+}
+
+getGDS <- function(name, destdir = tempdir()) {
+  l <- getGEO(name, destdir = destdir)
+  table <- slot(l, 'dataTable') # extracting all useful information on dataset
+  data <- Table(table)  # extracting table ID_REF | IDENTIFIER/SAMPLE | SAMPLE1 | ...
+  columnsMeta <- Columns(table) # phenoData
+  sampleNames <- as.vector(columnsMeta[["sample"]])
+  rownames <- as.vector(data[["ID_REF"]])
+  symbol <- as.vector(data[["IDENTIFIER"]])
+
+  data <- data[,sampleNames] # expression data
+  exprs <- as.matrix(data)
+  row.names(exprs) <- rownames
+
+  row.names(columnsMeta) <- sampleNames
+  columnsMeta <- columnsMeta[,!(colnames(columnsMeta) %in% c('sample'))] 
+  pData <- AnnotatedDataFrame(data.frame(columnsMeta, check.names = F))
+
+  fData <- data.frame(matrix(symbol, nrow(exprs), 1));
+  colnames(fData) <- "symbol"
+  fData <- AnnotatedDataFrame(fData)
+  featureNames(fData) <- res$rownames
+
+  ExpressionSet(assayData = exprs, phenoData = pData, featureData = fData)
+}
+
+getGSE <- function(name, destdir = tempdir()) {
+  es <- getGEO(name, AnnotGPL = T, destdir = destdir)[[1]]
+  featureData(es) <- featureData(es)[,grepl("symbol", varLabels(featureData(es)), ignore.case = T)]
+
+  phenoData(es) <- phenoData(es)[,grepl("characteristics", varLabels(phenoData(es)), ignore.case = T)
+                                  | (varLabels(phenoData(es)) %in% c("title", "id", "geo_accession"))]
+
+  es
+}
+
+
+getES <- function(name, type = NA, destdir = tempdir()) {
+  if (is.na(type)) {
+     type = substr(name, 1, 3)
+  }
+
+  if (type == 'GSE') {
+    es <- getGSE(name, destdir)
+  }
+  else if (type == "GDS") {
+    es <- getGDS(name, destdir)
+  }
+  else {
+    stop("Incorrect name or type of the dataset")
+  }
+  es
 }
