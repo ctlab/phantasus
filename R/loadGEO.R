61d509b5cf37032f1fabf6e872895a5eeb0729a9
baba-beda <darya.nightmare@gmail.com>
2017-04-27 15:10:33 +0300

    reformatted code of loadGEO

--- R/loadGEO.R
+++ R/loadGEO.R
@@ -7,44 +7,24 @@
 #' @export
 #' @import Biobase
 #' @import GEOquery
-loadGEO <- function(name, type) {
-  if (type == 'GSE') {
-    es.loaded <- getGEO(name, AnnotGPL = T, destdir = "/var/morpheus/cache")[[1]]
+loadGEO <- function(name, type = NA) {
+  assign("es", getES(name, type, destdir = "/var/morpheus/cache"), envir = parent.frame())
+  data <- as.matrix(exprs(es)); colnames(data) <- NULL; row.names(data) <- NULL
 
-    data <- as.matrix(exprs(es.loaded)); exprs <- data; colnames(data) <- NULL; row.names(data) <- NULL
+  pdata <- as.matrix(pData(es)); colnames(pdata) <- NULL; row.names(pdata) <- NULL
 
-    pd <- as.matrix(pData(es.loaded)); pData <- phenoData(es.loaded); colnames(pd) <- NULL; row.names(pd) <- NULL
-    participants <- colnames(es.loaded)
-    rownames <- rownames(es.loaded)
+  participants <- colnames(es)
+  rownames <- rownames(es)
 
-    fdata <- as.matrix(fData(es.loaded)[,grepl("symbol", varLabels(featureData(es.loaded)), ignore.case = T)])
-    colnames(fdata) <- NULL
-    row.names(fdata) <- NULL
+  fdata <- as.matrix(fData(es)[,grepl("symbol", varLabels(featureData(es)), ignore.case = T)])
+  colnames(fdata) <- NULL
+  row.names(fdata) <- NULL
 
-    varlabels <- if (ncol(fdata) > 0) "symbol" else NULL
-    res <- list(data = data, pdata = pd, participants = participants, symbol = if(is.null(varlabels)) NULL else fdata[,1], rownames = rownames, colMetaNames = colnames(pData(es.loaded)))
-  }
-  else {
-    l <- getGEO(name, destdir = "/var/morpheus/cache")
-    table <- slot(l, 'dataTable')
-    data <- Table(table)
-    rownames <- as.vector(data[["ID_REF"]])
-    symbol <- as.vector(data[["IDENTIFIER"]])
-    data <- data[,3:ncol(data)]
-    participants <- colnames(data)
-    columnsMeta <- Columns(table); row.names(columnsMeta) <- participants
-    columnsMeta <- as.matrix(columnsMeta[,!(colnames(columnsMeta) %in% c('sample'))]); pData <- AnnotatedDataFrame(data.frame(columnsMeta, check.names = F))
-    dm <- as.matrix(data); exprs <- dm; row.names(exprs) <- rownames; colnames(dm) <- NULL
-    colMetaNames <- colnames(columnsMeta); colnames(columnsMeta) <- NULL
-    res <- list(data = dm, pdata = columnsMeta, participants = participants, symbol = symbol, rownames = rownames, colMetaNames = colMetaNames)
-  }
+  varlabels <- if (ncol(fdata) > 0) "symbol" else NULL
+  fdata <- if (is.null(varlabels)) NULL else fdata[,1]
 
-  # good es here
+  res <- list(data = data, pdata = pdata, participants = participants, symbol = fdata, rownames = rownames, colMetaNames = colnames(pData(es.loaded)))
 
-  fData <- data.frame(matrix(res$symbol, nrow(res$data), 1)); colnames(fData) <- "symbol"
-  fData <- AnnotatedDataFrame(fData); featureNames(fData) <- res$rownames
-
-  assign("es", ExpressionSet(assayData = exprs, phenoData = pData, featureData = fData), envir = parent.frame())
   f <- tempfile(pattern = "gse", tmpdir = getwd(), fileext = ".bin")
   writeBin(protolite::serialize_pb(res), f)
   f
--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,45 @@ replacenas <- function(data, replacena) {
   data <- data[left.rows,]
   data
 }
+
+getGDS <- function(name, destdir = tempdir()) {
+  l <- getGEO(name, destdir = destdir)
+  table <- slot(l, 'dataTable') # extracting all useful information on dataset
+  data <- Table(table)  # extracting table ID_REF | IDENTIFIER/SAMPLE | SAMPLE1 | ...
+  columnsMeta <- Columns(table) # phenoData
+  sampleNames <- as.vector(columnsMeta[["sample"]])
+  rownames <- as.vector(data[["ID_REF"]])
+  symbol <- as.vector(data[["IDENTIFIER"]])
+
+  data <- data[,sampleNames] # expression data
+  exprs <- as.matrix(data)
+  row.names(exprs) <- rownames
+
+  row.names(columnsMeta) <- sampleNames
+  columnsMeta <- columnsMeta[,!(colnames(columnsMeta) %in% c('sample'))] 
+  pData <- AnnotatedDataFrame(data.frame(columnsMeta, check.names = F))
+
+  fData <- data.frame(matrix(symbol, nrow(exprs), 1));
+  colnames(fData) <- "symbol"
+  fData <- AnnotatedDataFrame(fData)
+  featureNames(fData) <- res$rownames
+
+  ExpressionSet(assayData = exprs, phenoData = pData, featureData = fData)
+}
+
+getES <- function(name, type = NA, destdir = tempdir()) {
+  if (is.na(type)) {
+     type = substr(name, 1, 3)
+  }
+
+  if (type == 'GSE') {
+    es <- getGEO(name, AnnotGPL = T, destdir = destdir)[[1]]
+  }
+  else if (type == "GDS") {
+    es <- getGDS(name, destdir)
+  }
+  else {
+    stop("Incorrect name or type of the dataset")
+  }
+  es
+}
