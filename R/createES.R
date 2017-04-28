0890d367a9b941f152c8bb89d374f4472f49ff48
baba-beda <darya.nightmare@gmail.com>
2017-04-28 14:48:21 +0300

    simplified creating es

--- R/createES.R
+++ R/createES.R
@@ -9,21 +9,16 @@
 #' @param symbol gene symbol
 #' @export
 #' @import Biobase
-createES <- function(data, pData, labelDescription, colNames, rowNames, symbol) {
+createES <- function(data, pData, varLabels, fData, fvarLabels) {
   exprs <- t(data)
   colnames(exprs) <- colNames
   truePData <- pData
-  pd <- data.frame(truePData, row.names = colNames)
-  names(pd) <- labelDescription
-  phenoData <- AnnotatedDataFrame(pd)
-  if (is.null(symbol) || length(symbol) == 0) {
-    symbol <- 1:nrow(exprs)
-  }
-  featureData <- AnnotatedDataFrame(data.frame(symbol))
-  if (is.null(rowNames) || length(rowNames) == 0) {
-    rowNames <- 1:nrow(exprs)
-  }
-  featureNames(featureData) <- rowNames
+  phenoData <- AnnotatedDataFrame(data.frame(pData))
+  varLabels(phenoData) <- varLabels
+  
+  featureData <- AnnotatedDataFrame(data.frame(fData))
+  varLabels(featureData) <- fvarLabels
+ 
   es <- ExpressionSet(assayData = exprs, phenoData=phenoData, featureData = featureData)
   assign("es", es, envir = parent.frame())
   es
