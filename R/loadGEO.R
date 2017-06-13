cbca9611674d0ce34fe9b2e4d5f850c20e49dc6d
baba-beda <darya.nightmare@gmail.com>
2017-06-13 17:42:47 +0300

    added support for several files in one gse-identifier

--- R/loadGEO.R
+++ R/loadGEO.R
@@ -8,27 +8,42 @@
 #' @import Biobase
 #' @import GEOquery
 loadGEO <- function(name, type = NA) {
-  es <- getES(name, type, destdir = "/var/phantasus/cache")
-  assign("es", es, envir = parent.frame())
-  data <- as.matrix(exprs(es)); colnames(data) <- NULL; row.names(data) <- NULL
+  cacheDir <- getOption("phantasusCacheDir")
+  if (is.null(cacheDir)) {
+    cacheDir <- tempdir()
+  } else if (!dir.exists(cacheDir)) {
+    dir.create(cacheDir)
+  }
+  ess <- getES(name, type, destdir = cacheDir)
 
-  pdata <- as.matrix(pData(es)); colnames(pdata) <- NULL; row.names(pdata) <- NULL
+  writeToFile <- function(es) {
+    data <- as.matrix(exprs(es)); colnames(data) <- NULL; row.names(data) <- NULL
 
-  participants <- colnames(es)
-  rownames <- rownames(es)
+    pdata <- as.matrix(pData(es)); colnames(pdata) <- NULL; row.names(pdata) <- NULL
 
-  fdata <- as.matrix(fData(es))
-  colnames(fdata) <- NULL
-  row.names(fdata) <- NULL
+    participants <- colnames(es)
+    rownames <- rownames(es)
 
-  res <- list(data = data, pdata = pdata,
-              fdata = fdata, rownames = rownames,
-              colMetaNames = varLabels(phenoData(es)),
-              rowMetaNames = varLabels(featureData(es)))
+    fdata <- as.matrix(fData(es))
+    colnames(fdata) <- NULL
+    row.names(fdata) <- NULL
 
-  f <- tempfile(pattern = "gse", tmpdir = getwd(), fileext = ".bin")
-  writeBin(protolite::serialize_pb(res), f)
-  f
+    res <- list(data = data, pdata = pdata,
+                fdata = fdata, rownames = rownames,
+                colMetaNames = varLabels(phenoData(es)),
+                rowMetaNames = varLabels(featureData(es)))
+
+    f <- tempfile(pattern = "gse", tmpdir = getwd(), fileext = ".bin")
+    writeBin(protolite::serialize_pb(res), f)
+    f
+  }
+
+  files <- list()
+  for(i in 1:length(ess)) {
+    assign(paste("es_", i), ess[[i]], envir = parent.frame())
+    files[[i]] <- writeToFile(ess[[i]])
+  }
+  jsonlite::toJSON(files)
 }
 
 getGDS <- function(name, destdir = tempdir()) {
@@ -45,7 +60,7 @@ getGDS <- function(name, destdir = tempdir()) {
   row.names(exprs) <- rownames
 
   row.names(columnsMeta) <- sampleNames
-  # columnsMeta <- columnsMeta[,!(colnames(columnsMeta) %in% c('sample'))] 
+  # columnsMeta <- columnsMeta[,!(colnames(columnsMeta) %in% c('sample'))]
   pData <- AnnotatedDataFrame(data.frame(columnsMeta, check.names = F))
 
   fData <- data.frame(matrix(symbol, nrow(exprs), 1));
@@ -53,47 +68,54 @@ getGDS <- function(name, destdir = tempdir()) {
   fData <- AnnotatedDataFrame(fData)
   featureNames(fData) <- rownames
 
-  ExpressionSet(assayData = exprs, phenoData = pData, featureData = fData)
+  list(ExpressionSet(assayData = exprs, phenoData = pData, featureData = fData))
 }
 
 getGSE <- function(name, destdir = tempdir()) {
-  es <- getGEO(name, AnnotGPL = T, destdir = destdir)[[1]]
-  featureData(es) <- featureData(es)[,grepl("symbol", fvarLabels(es), ignore.case = T)]
-
-  phenoData(es) <- phenoData(es)[,grepl("characteristics", varLabels(es), ignore.case = T)
-                                  | (varLabels(es) %in% c("title", "id", "geo_accession"))]
-
-  chr <- varLabels(es)[grepl("characteristics", varLabels(es), ignore.case = T)]
+  ess <- getGEO(name, AnnotGPL = T, destdir = destdir)
 
   take <- function(x, n) {
     sapply(x, function(x) { x[[n]] })
   }
+
   rename <- function(prevName, x) {
     splitted <- strsplit(x, ": ")
     sumlength <- sum(sapply(as.vector(splitted), length))
     if (sumlength != 2 * length(x)) {
-       return(list(name = prevName, x = x))
+      return(list(name = prevName, x = x))
     }
     splittedFirst <- unique(take(splitted, 1))
     if (length(splittedFirst) == 1) {
-       res = list(name = splittedFirst[1], x = take(splitted, 2))
+      res = list(name = splittedFirst[1], x = take(splitted, 2))
     }
     else {
-       res = list(name = prevName, x = x)
+      res = list(name = prevName, x = x)
     }
     res
   }
 
-  renamed <- lapply(chr, function(x) { rename(x, as.vector(pData(es)[,x])) })
-  phenoData(es) <- phenoData(es)[, !(varLabels(es) %in% chr)]
-  pData(es)[,take(renamed,1)] <- take(renamed,2)
-  es
+  processInputES <- function(es) {
+    featureData(es) <- featureData(es)[,grepl("symbol", fvarLabels(es), ignore.case = T)]
+
+    phenoData(es) <- phenoData(es)[,grepl("characteristics", varLabels(es), ignore.case = T)
+                                   | (varLabels(es) %in% c("title", "id", "geo_accession"))]
+
+    chr <- varLabels(es)[grepl("characteristics", varLabels(es), ignore.case = T)]
+
+    renamed <- lapply(chr, function(x) { rename(x, as.vector(pData(es)[,x])) })
+    phenoData(es) <- phenoData(es)[, !(varLabels(es) %in% chr)]
+    pData(es)[,take(renamed,1)] <- take(renamed,2)
+
+    es
+  }
+
+  lapply(ess, processInputES)
 }
 
 
 getES <- function(name, type = NA, destdir = tempdir()) {
   if (is.na(type)) {
-     type = substr(name, 1, 3)
+    type = substr(name, 1, 3)
   }
 
   if (type == 'GSE') {
@@ -107,3 +129,4 @@ getES <- function(name, type = NA, destdir = tempdir()) {
   }
   es
 }
+
