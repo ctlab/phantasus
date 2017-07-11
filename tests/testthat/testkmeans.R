d40c41554a5b128a9b6a12542df1559faf7a1528
baba-beda <darya.nightmare@gmail.com>
2017-07-11 19:51:08 +0300

    test es moved to testdata

--- tests/testthat/testkmeans.R
+++ tests/testthat/testkmeans.R
@@ -1,6 +1,6 @@
 context("Kmeans")
 
 test_that("kmeans finishes with result", {
-  load(file = "../../data/GSE27112-GPL6103.rda")
+  load(file = "testdata/GSE27112-GPL6103.rda")
   expect_is(phantasus::kmeans(es, k = 10), "json")
 })
--- tests/testthat/testpcaPlot.R
+++ tests/testthat/testpcaPlot.R
@@ -1,6 +1,6 @@
 context("PCA Plot")
 
 test_that("pcaPlot finishes with result", {
-  load(file = "../../data/GSE27112-GPL6103.rda")
+  load(file = "testdata/GSE27112-GPL6103.rda")
   expect_is(pcaPlot(es), "json")
 })
