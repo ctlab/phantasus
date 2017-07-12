10c6f07c6f8ce92294a561cb782719c856d96f58
baba-beda <darya.nightmare@gmail.com>
2017-07-12 20:32:14 +0300

    used incorrect  es in testutilfunctions

--- tests/testthat/testutilfunctions.R
+++ tests/testthat/testutilfunctions.R
@@ -2,6 +2,6 @@ context("Util functions")
 library(rUtils)
 
 test_that("prepareData replaces all NAs and filters fully NA rows", {
-  data <- prepareData(es)
+  data <- prepareData(read.gct("testdata/centers.gct"))
   expect_equal(nrow(data), 5)
 })
