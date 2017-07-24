29fb0c72d262326781ca82f6d4ca99d2c55ba83c
baba-beda <darya.nightmare@gmail.com>
2017-07-24 18:09:00 +0300

    exit status added

--- inst/test_js.sh
+++ inst/test_js.sh
@@ -4,6 +4,13 @@ npm install
 
 R -e "phantasus::servePhantasus('0.0.0.0', 8000, cacheDir = file.path(getwd(), 'jasmine', 'cache'))" &
 karma start my.conf.js --single-run
+RETVAL=$?
 
 cd
 
+if [ $RETVAL -eq 0 ]
+then
+    echo "Jasmine tests are successfully passed"
+else
+    echo "Tests failed" >&2
+fi
