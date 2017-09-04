3fa599012679518d6149d08dc467c6ac090952d7
baba-beda <darya.nightmare@gmail.com>
2017-09-04 18:45:24 +0300

    let's just expect warnings in tests of unreal datasets

--- tests/testthat/testloadGEO.R
+++ tests/testthat/testloadGEO.R
@@ -20,8 +20,8 @@ test_that("checkGPLs counts gpls correctly", {
   expect_equal(fromJSON(checkGPLs("GDS4885")), c("GDS4885"))
   expect_length(fromJSON(checkGPLs("GSE27112")), 2)
   expect_length(fromJSON(checkGPLs("GSE10000")), 2)
-  expect_length(fromJSON(checkGPLs("GDS101")), 0)
-  expect_length(fromJSON(checkGPLs("GSE201")), 0)
+  expect_warning(checkGPLs("GSE101"))
+  expect_warning(checkGPLs("GSE201"))
 
   options(phantasusMirrorPath = NULL)
 })
