de96c12561264208e9424512a78ab6a597c29ef5
baba-beda <darya.nightmare@gmail.com>
2017-09-13 19:37:04 +0300

    added tutorial as html to static files

--- inst/update_static.sh
+++ inst/update_static.sh
@@ -2,8 +2,6 @@
 
 cd ../inst-raw
 
-git submodule init && git submodule update
-
 cp phantasus.js/js/phantasus-latest.min.js phantasus.js/js/phantasus-external-latest.min.js ../inst/www/phantasus.js/js/
 cp -r phantasus.js/jasmine ../inst/www/phantasus.js/
 cp phantasus.js/my.conf.js phantasus.js/package.json ../inst/www/phantasus.js/
