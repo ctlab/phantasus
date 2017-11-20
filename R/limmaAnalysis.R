827f546e36fe2ae86c8daa82105a54e2351603b1
Alexey Sergushichev <alsergbox@gmail.com>
2017-11-21 00:24:29 +0300

    limma code simplified

--- R/limmaAnalysis.R
+++ R/limmaAnalysis.R
@@ -1,3 +1,26 @@
+limmaAnalysisImpl <- function(es, rows, columns, fieldValues) {
+    fieldValues <- fieldValues[columns]
+
+    fieldValues <- replace(fieldValues, fieldValues == "", NA)
+
+    es.copy <- es[rows, columns]
+    es.copy$Comparison <- fieldValues
+    fData(es.copy) <- data.frame(row.names = rownames(es.copy))
+
+    es.copy <- es.copy[, !is.na(es.copy$Comparison)]
+
+    es.design <- stats::model.matrix(~0 + Comparison, data = pData(es.copy))
+
+    fit <- lmFit(es.copy, es.design)
+
+    A <- NULL; B <- NULL
+    fit2 <- contrasts.fit(fit, makeContrasts(ComparisonB - ComparisonA, levels = es.design))
+    fit2 <- eBayes(fit2)
+    de <- topTable(fit2, adjust.method = "BH", number = Inf)
+    de <- de[row.names(fData(es.copy)), ]
+    de
+}
+
 #' Differential Expression analysis.
 #'
 #' \code{limmaAnalysis} performs differential expression analysis
@@ -32,31 +55,8 @@ limmaAnalysis <- function(es, rows = c(), columns = c(), fieldValues) {
     rows <- getIndicesVector(rows, nrow(exprs(es)))
     columns <- getIndicesVector(columns, ncol(exprs(es)))
 
-    fieldValues <- fieldValues[columns]
+    de <- limmaAnalysisImpl(es, rows=rows, columns = columns, fieldValues)
 
-    fieldName <- "Comparison"
-    fieldValues <- replace(fieldValues, fieldValues == "", NA)
-
-    new.pdata <- pData(es)[columns, , drop=F]
-    new.pdata[[fieldName]] <- as.factor(fieldValues)
-    new.pdata <- new.pdata[!is.na(new.pdata[[fieldName]]), ]
-    new.sampleNames <- row.names(new.pdata)
-
-    es.copy <- es[rows, new.sampleNames]
-    pData(es.copy) <- new.pdata
-    fData(es.copy) <- data.frame(row.names = rownames(es.copy))
-
-    es.design <- stats::model.matrix(~0 + Comparison, data = pData(es.copy))
-    colnames(es.design) <- gsub(pattern = fieldName, replacement = "",
-                                x = make.names(colnames(es.design)))
-
-    fit <- lmFit(es.copy, es.design)
-
-    A <- NULL; B <- NULL
-    fit2 <- contrasts.fit(fit, makeContrasts(B - A, levels = es.design))
-    fit2 <- eBayes(fit2)
-    de <- topTable(fit2, adjust.method = "BH", number = Inf)
-    de <- de[row.names(fData(es.copy)), ]
     f <- tempfile(pattern = "de", tmpdir = getwd(), fileext = ".bin")
     writeBin(protolite::serialize_pb(as.list(de)), f)
     jsonlite::toJSON(f)
--- tests/testthat/testlimmaAnalysis.R
+++ tests/testthat/testlimmaAnalysis.R
@@ -12,3 +12,12 @@ test_that("limmaAnalysis works when there is only one phenotype attribute", {
     expect_is(limmaAnalysis(es,
                             fieldValues = c(rep("A", 3), rep("B", 2))), "json")
 })
+
+
+test_that("limmaAnalysisImpl works", {
+    load(file = "testdata/GSE27112-GPL6103.rda")
+    de <- limmaAnalysisImpl(es, rows=seq_len(nrow(es)), columns=seq_len(ncol(es)),
+                            fieldValues = c(rep("A", 2), rep("B", 2), NA))
+
+    expect_equal(de$logFC[1], mean(exprs(es)[1, 3:4]) - mean(exprs(es)[1, 1:2]))
+})
