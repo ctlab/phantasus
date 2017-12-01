fdd67bf7f29692b715c0cc78c8cc9c74b8ae000d
Alexey Sergushichev <alsergbox@gmail.com>
2017-12-01 15:55:50 +0300

    Better DE table handling for Shiny GAM

--- R/shinyGAM.R
+++ R/shinyGAM.R
@@ -1,21 +1,22 @@
-#' @export
-shinyGAMAnalysis <- function(fData, fvarLabels, orgCode) {
+#' Constructs data frame with gene annotations and submits it into Shiny GAM web-server
+#' @return URL for Shiny GAM
+#' @import httr
+shinyGAMAnalysis <- function(fData, fvarLabels) {
     featureData <- data.frame(fData)
     colnames(featureData) <- fvarLabels
     de <- featureData
 
 
-    #deFile <- tempfile()
-    deFile <- "/tmp/asd.txt"
-    write.table(de, deFile, sep="\t", row.names=T, col.names=NA)
+    deFile <- tempfile()
+    write.table(de, deFile, sep="\t", row.names=F, col.names=T)
 
-    # :TODO: get ssl.verifypeer from option!!!
+    # :ToDo: deal with bad certificates and remove ssl_verifypeer option
     r <- httr::POST(url="https://artyomovlab.wustl.edu/upload",
                body=readBin(deFile, what="raw", n=20e6),
                httr::config(ssl_verifypeer = FALSE))
     httr::stop_for_status(r)
     key <- httr::content(r, as="text", encoding="UTF-8")
 
-    shinyGAMUrl <- sprintf("https://artyomovlab.wustl.edu/shiny/gam/?oranism=%s&geneDE_key=%s", orgCode, key)
+    shinyGAMUrl <- sprintf("https://artyomovlab.wustl.edu/shiny/gam/?geneDE_key=%s", key)
     return(jsonlite::toJSON(shinyGAMUrl))
 }
