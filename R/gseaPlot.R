b688b3edb347fdece123686d0eb77838d1776113
Alexey Sergushichev <alsergbox@gmail.com>
2018-04-23 11:46:54 +0200

    fixed names in gseaPlot

--- R/gseaPlot.R
+++ R/gseaPlot.R
@@ -8,25 +8,28 @@
 #' @return path to an svg file
 #' @importFrom fgsea plotEnrichment fgsea
 #' @importFrom ggplot2 ggtitle
-gseaPlot <- function(fData, fVarLabels, rankBy, selectedGenes, width, height) {
+gseaPlot <- function(fData, fvarLabels, rankBy, selectedGenes, width, height) {
     featureData <- data.frame(fData)
     colnames(featureData) <- fvarLabels
+
     ranks <- setNames(featureData[, rankBy], as.character(selectedGenes))
 
     pathway <- as.character(selectedGenes)
 
+    fgseaRes <- fgsea(list(pathway), ranks, nperm=2000, nproc=1)
 
-    fgseaRes <- fgsea(list(pathway), ranks, nperm=25000, nproc=1)
-
-    pvalString <- sprintf("p-value %s",
-    if (fgseaRes$nMoreExtreme == 0) {
-        " < 1e-4"
+    pvalString <- if (fgseaRes$nMoreExtreme == 0) {
+        " < 1e-3"
     } else {
         sprintf(" = %g", fgseaRes$pval)
-    })
+    }
 
-    p <- plotEnrichment(pathway, ranks) + ggtitle(pvalString)
+    labelString <- sprintf("p-value %s, NES = %s", pvalString, fgseaRes$NES)
+
+
+    p <- plotEnrichment(pathway, ranks) + ggtitle(labelString)
     f <- tempfile(pattern = "enrichment", tmpdir = getwd(), fileext = ".svg")
-    ggsave(p, filename = f, width=width, height=height)
+    ggsave(p, filename = f, width=6, height=4)
+    # ggsave(p, filename = f, width=width, height=height)
     jsonlite::toJSON(f)
 }
