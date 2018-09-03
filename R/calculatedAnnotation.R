211f56945906e818d9698c5be2fd9570c02eafc8
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-09-03 13:38:19 +0300

    Annotation name implemented

--- R/calculatedAnnotation.R
+++ R/calculatedAnnotation.R
@@ -6,22 +6,23 @@
 #' @param operation Name of the operation to perform calculation
 #' @param columns List of specified columns' indices (optional), indices start from 0#'
 #' @param rows List of specified rows' indices (optional), indices start from 0
+#' @param name Name of the new annotation
 #' @param isColumns Apply fn to columns
 #'
 #' @import Biobase
 #'
 
-calculatedAnnotation <- function (es, operation, rows = c(), columns = c(), isColumns = FALSE) {
+calculatedAnnotation <- function (es, operation, rows = c(), columns = c(), isColumns = FALSE, name = NULL) {
     rows <- getIndicesVector(rows, nrow(exprs(es)))
     columns <- getIndicesVector(columns, ncol(exprs(es)))
 
     fn <- tolower(operation)
     if (!isColumns) {
-        fData(es)[[operation]] <- NA
-        fData(es)[[operation]][rows] <- apply(exprs(es[rows,columns]), 1, fn)
+        fData(es)[[name]] <- NA
+        fData(es)[[name]][rows] <- apply(exprs(es[rows,columns]), 1, fn)
     } else {
-        phenoData(es)[[operation]] <- NA
-        phenoData(es)[[operation]][columns] <- apply(exprs(es[rows,columns]), 2, fn)
+        phenoData(es)[[name]] <- NA
+        phenoData(es)[[name]][columns] <- apply(exprs(es[rows,columns]), 2, fn)
     }
 
     assign("es", es, envir = parent.frame())
