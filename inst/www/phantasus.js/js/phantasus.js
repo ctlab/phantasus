cd933d133bbc62e8ffe3d646df126d8fac30cc17
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-09-28 20:21:29 +0300

    RC

--- inst/www/phantasus.js/js/phantasus.js
+++ inst/www/phantasus.js/js/phantasus.js
@@ -17918,10 +17918,18 @@ phantasus.PcaPlotTool.prototype = {
       }
 
       if (colorByVector) {
+        var colorModel = _this.project.getColumnColorModel();
         var uniqColors = {};
         color = _.map(phantasus.VectorUtil.toArray(colorByVector), function (value) {
           if (!uniqColors[value]) {
-            uniqColors[value] = phantasus.VectorColorModel.CATEGORY_ALL[_.size(uniqColors) % 60];
+            if (colorModel.containsDiscreteColor(colorByVector, value)
+              && colorByVector.getProperties().get(phantasus.VectorKeys.DISCRETE)) {
+              uniqColors[value] = colorModel.getMappedValue(colorByVector, value);
+            } else if (colorModel.isContinuous(colorByVector)) {
+              uniqColors[value] = colorModel.getContinuousMappedValue(colorByVector, value);
+            } else {
+              uniqColors[value] = phantasus.VectorColorModel.CATEGORY_ALL[_.size(uniqColors) % 60];
+            }
           }
 
           return uniqColors[value]
