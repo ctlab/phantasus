138575a4c3e960d2b37f3d09abe1f0b532d2ac5b
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-10-02 17:13:01 +0300

    Complex arguments

--- R/exportDatasetHistory.R
+++ R/exportDatasetHistory.R
@@ -1,24 +1,41 @@
 rootFn <- c('loadGEO', 'loadPreloaded', 'createES')
+ignoredArgs <- c('es')
 
-exportDatasetHistory <- function (sessionName, esVariable="es") {
-    ocpuRoot <- Sys.getenv('OCPU_MASTER_HOME')
-    if (length (ocpuRoot) <= 1) {
-        ocpuRoot <- tempdir()
-    }
-    REvalPath <- paste(ocpuRoot, 'ocpu-store',sessionName, '.REval', sep=.Platform$file.sep)
+exportDatasetHistory <- function (sessionName, esVariable="es", leaf = T) {
+    ocpuRoot <- strsplit(getwd(), 'ocpu-temp')[[1]][1]
+    sessionPath <- paste(ocpuRoot, 'ocpu-store', sessionName, sep=.Platform$file.sep)
+    RDataPath <- paste(sessionPath, '.RData', sep=.Platform$file.sep)
+    REvalPath <- paste(sessionPath, '.REval', sep=.Platform$file.sep)
     rds <- readRDS(REvalPath)
+    env <- new.env()
+    loadedVariables <- load(RDataPath, envir = env)
     parsed <- parse(text=rds[[1]]$src)
     fn <- parsed[[1]][[1]]
+    complexArguments <- "";
+    for(i in 2:length(parsed[[1]])) {
+        argumentName <- toString(parsed[[1]][[i]])
+        if (argumentName %in% loadedVariables) {
+            complexArgument <- paste(argumentName, ' <- ', toString(env[[argumentName]]))
+            complexArguments <- paste(complexArguments, complexArgument, sep='\n')
+        }
+    }
+    myHistory <- paste(complexArguments, rds[[1]]$src, sep='\n')
+
     if (!is.element(toString(fn), rootFn)) {
         parent <- parsed[[1]]$es[[2]]
         parentVar <- parsed[[1]]$es[[3]]
-        parentHistory <- exportDatasetHistory(parent, parentVar)
-        ourHistory <- paste(parentHistory, rds[[1]]$src, sep='\n')
-        return(jsonlite::toJSON(ourHistory))
+        parentHistory <- exportDatasetHistory(parent, parentVar, leaf = F)
+        ourHistory <- paste(parentHistory, myHistory, sep='\n')
+        if (leaf) {
+            return(jsonlite::toJSON(ourHistory))
+        }
+        return(ourHistory)
     }
-    rds[[1]]$src
-
+    if (leaf) {
+        return(jsonlite::toJSON(myHistory))
+    }
+    myHistory
 }
 playground <- function () {
-    exportDatasetHistory('x093ddc6795');
+    exportDatasetHistory('x04fdf2b9ebbbda');
 }
