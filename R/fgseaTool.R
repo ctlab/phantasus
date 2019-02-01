a72420d4d7c582546b9a6e3a1daf24248cf8637b
Vladislav Kamenev <mazahakaforever@ya.ru>
2019-02-01 19:44:11 +0300

    FGSEA tool

---
+++ R/fgseaTool.R
@@ -0,0 +1,33 @@
+availableFGSEADatabases <- function () {
+    cacheDir <- getOption("phantasusCacheDir")
+
+    dbDir <- file.path(cacheDir, 'fgsea')
+    columnFiles <- list.files(dbDir, full.names = FALSE)
+    columnFiles <- sapply(strsplit(as.character(columnFiles), "\\."), "[[", 1)
+
+    return (jsonlite::toJSON(columnFiles))
+}
+
+performFGSEA <- function (dbName, ranks) {
+    cacheDir <- getOption("phantasusCacheDir")
+
+    dbDir <- file.path(cacheDir, 'fgsea')
+    db <- readRDS(file.path(dbDir, paste(dbName, '.rds', sep='')))
+    pathways <- lapply(lapply(split(db, db$pathName), '[[', 'geneID'), as.character)
+    rranks <- ranks$ranks
+    names(rranks) <- ranks$genes
+
+    assign('db', db, envir = parent.frame())
+
+    return (jsonlite::toJSON(fgsea(pathways, rranks, nperm = 1000)))
+}
+
+queryPathway <- function (dbName, pathwayName) {
+    cacheDir <- getOption("phantasusCacheDir")
+
+    dbDir <- file.path(cacheDir, 'fgsea')
+    db <- readRDS(file.path(dbDir, paste(dbName, '.rds', sep='')))
+    filteredDb <- db[db$pathName == pathwayName,]
+
+    jsonlite::toJSON(filteredDb$geneID)
+}
