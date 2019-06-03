f719054645e1563cfb03ca53bea1096aa3c988cd
petrovich <mazahakaforever@ya.ru>
2019-06-03 19:06:17 +0300

    Update JS

--- inst/www/phantasus.js/js/phantasus.js
+++ inst/www/phantasus.js/js/phantasus.js
@@ -19149,6 +19149,80 @@ phantasus.ProbeDebugTool.prototype = {
   }
 };
 
+
+phantasus.ReproduceTool = function (project) {
+  var _this = this;
+
+  this.$el = $('<div class="container-fluid">'
+    + '<div class="row">'
+    + '<div data-name="controlPane" class="col-xs-2">'
+    + '<button id="copy_btn" class="btn btn-default btn-sm">Copy to clipboard</button>'
+    + '<button id="save_env_btn" style="margin-top:15px; display: none" class="btn btn-default btn-sm">Save environment</button>'
+    + '</div>'
+    + '<div class="col-xs-10"><div data-name="codePane"></div></div>'
+    + '<div class=""></div>'
+    + '</div></div>');
+
+  this.codePane = this.$el.find('[data-name=codePane]');
+  this.copyBtn = this.$el.find('#copy_btn');
+  this.saveEnvBtn = this.$el.find('#save_env_btn');
+
+  this.copyBtn.on('click', function () {
+    _this.codePane.find('textarea').select();
+    document.execCommand('copy');
+    _this.codePane.find('textarea').blur();
+  });
+
+  var $dialog = $('<div style="background:white;" title="' + this.toString() + '"></div>');
+  this.$el.appendTo($dialog);
+  $dialog.dialog({
+    close: function (event, ui) {
+      $dialog.dialog('destroy').remove();
+      event.stopPropagation();
+    },
+
+    resizable: true,
+    height: 620,
+    width: 950
+  });
+
+  this.execute(project);
+};
+phantasus.ReproduceTool.prototype = {
+  experimentalWarning: true,
+  toString: function () {
+    return 'Reproduce in R code';
+  },
+  execute: function (project) {
+    this.codePane.empty();
+    phantasus.Util.createLoadingEl().appendTo(this.codePane);
+
+    var _this = this;
+    var dataset = project.getFullDataset();
+
+    dataset.getESSession().then(function (esSession) {
+      ocpu.call('reproduceInR/print', {
+        sessionName: esSession.key
+      }, function (newSession) {
+        var text = JSON.parse(newSession.txt)[0];
+        text = '# docker image: dzenkova/phantasus:' + PHANTASUS_BUILD + '\n' + text;
+
+        var textarea = $('<textarea style="height: 100%; width: 100%; resize: none;" id="codeArea" readonly>' + text + '</textarea>');
+        _this.codePane.empty();
+        textarea.appendTo(_this.codePane);
+
+        _this.saveEnvBtn.off();
+        _this.saveEnvBtn.on('click', function () {
+          window.open(newSession.getLoc() + 'R/env/rda', '_blank');
+        });
+        _this.saveEnvBtn.css('display', 'block');
+      }).fail(function () {
+        throw new Error('Failed to reproduce in R. See console');
+      });
+    });
+  }
+};
+
 phantasus.SaveDatasetTool = function () {
 };
 phantasus.SaveDatasetTool.prototype = {
@@ -21225,6 +21299,15 @@ phantasus.ActionManager = function () {
       }
     })
   }
+
+  this.add({
+    name: phantasus.ReproduceTool.prototype.toString(),
+    cb: function (options) {
+      new phantasus.ReproduceTool(
+        options.heatMap.getProject()
+      );
+    }
+  });
 
   this.add({
     name: 'Submit to Enrichr',
@@ -31803,7 +31886,8 @@ phantasus.HeatMap = function (options) {
           'Pathway analysis',
           'Submit to Shiny GAM',
           'DEBUG: Probe Debug Tool',
-          'DEBUG: Expose project'],
+          'DEBUG: Expose project',
+          phantasus.ReproduceTool.prototype.toString()],
         View: ['Zoom In', 'Zoom Out', null, 'Fit To Window', 'Fit Rows To Window', 'Fit Columns To Window', null, '100%', null, 'Options'],
         Edit: [
           'Copy Selected Dataset',
