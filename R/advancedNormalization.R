6409ba35fdce52b8cca5c55373642b25fed19cdd
Maksim Kleverov <klevermx@gmail.com>
2023-04-19 09:58:33 +0300

    voom normalization first version

--- R/advancedNormalization.R
+++ R/advancedNormalization.R
@@ -22,5 +22,39 @@ tmmNormalization <- function(es, fieldName, logratioTrim, sumTrim) {
     rownames <- rownames(es.copy)
 
     writeBin(protolite::serialize_pb(list(data = data, colMetaNames = varLabels(es.copy))), f)
-   return( jsonlite::toJSON(f))
+    return( jsonlite::toJSON(f))
+}
+
+
+
+voomNormalization <- function(es, fieldNames){
+    if (length(fieldNames) == 0 ){
+        design <- NULL
+    }
+    if (! "design" %in% ls()){
+        fieldNames <- unlist(fieldNames)
+        pdata <- pData(es)[,fieldNames]
+        pdata[,fieldNames] <-  lapply(fieldNames, function(x){
+            curFact <- as.factor(pdata[,x])
+            curFact
+        })
+        design <- model.matrix(~ 0 + ., data = pdata)
+        if(qr(design)$rank < ncol(design)){
+            stop("Error: redundancy of model parameters appears. Try to exclude nested factors.")
+        }
+    }
+
+    es.copy <- es
+    voom_counts <- voom(counts = exprs(es.copy), design = design)
+    exprs(es.copy) <- voom_counts$E
+
+    assign("es", es.copy, envir = parent.frame())
+    f <- tempfile(pattern = "voom_counts", tmpdir = getwd(), fileext = ".bin")
+    data <- as.matrix(exprs(es.copy))
+    colnames(data) <- NULL
+    row.names(data) <- NULL
+    rownames <- rownames(es.copy)
+
+    writeBin(protolite::serialize_pb(list(data = data, colMetaNames = varLabels(es.copy))), f)
+    return( jsonlite::toJSON(f))
 }
