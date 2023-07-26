b60858ab0ed5bcf45ba8af0310a2a6e66eb96ea3
Maksim Kleverov <klevermx@gmail.com>
2023-07-26 11:13:57 -0500

    optional filter and cpm for voom and tmm

--- R/advancedNormalization.R
+++ R/advancedNormalization.R
@@ -3,7 +3,7 @@
 #' @import limma
 #' @import edgeR
 #'
-tmmNormalization <- function(es, fieldName, logratioTrim, sumTrim) {
+tmmNormalization <- function(es, fieldName, logratioTrim, sumTrim, convertCPM = FALSE) {
     if (fieldName != "None"){
         fieldValues <- es[[fieldName]]
     } else {
@@ -12,7 +12,10 @@ tmmNormalization <- function(es, fieldName, logratioTrim, sumTrim) {
     es.copy <- es
     count_factors <- DGEList(counts = exprs(es.copy), group = fieldValues)
     count_factors <- calcNormFactors(count_factors, logratioTrim = as.numeric(logratioTrim), sumTrim = as.numeric(sumTrim))
-    exprs(es.copy) <- cpm(count_factors)
+    if (convertCPM){
+        count_factors <- cpm(count_factors)
+    }
+    exprs(es.copy) <- count_factors
 
     assign("es", es.copy, envir = parent.frame())
     f <- tempfile(pattern = "norm_counts", tmpdir = getwd(), fileext = ".bin")
@@ -27,10 +30,16 @@ tmmNormalization <- function(es, fieldName, logratioTrim, sumTrim) {
 
 
 
-voomNormalization <- function(es, designData){
+voomNormalization <- function(es, designData, filterByExp = FALSE){
     designMatrix <- getDesignMatrix(designData)
     es.copy <- es
+    keep <- TRUE
+    if (filterByExp){
+        keep <- filterByExpr(exprs(es.copy), designMatrix)
+    }
+    es.copy <- es.copy[keep,]
     voom_counts <- voom(counts = exprs(es.copy), design = designMatrix)
+
     exprs(es.copy) <- voom_counts$E
 
     assign("es", es.copy, envir = parent.frame())
@@ -38,8 +47,7 @@ voomNormalization <- function(es, designData){
     data <- as.matrix(exprs(es.copy))
     colnames(data) <- NULL
     row.names(data) <- NULL
-    rownames <- rownames(es.copy)
 
-    writeBin(protolite::serialize_pb(list(data = data, colMetaNames = varLabels(es.copy))), f)
+    writeBin(protolite::serialize_pb(list(data = data, keep = which(keep)-1)), f)
     return( jsonlite::toJSON(f))
 }
