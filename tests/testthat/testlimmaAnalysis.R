b6dac95b2bd2773efff2081d8138e1a18833fc7e
Maksim Kleverov <klevermx@gmail.com>
2023-08-01 19:57:28 -0500

    fix limma tests

--- tests/testthat/testlimmaAnalysis.R
+++ tests/testthat/testlimmaAnalysis.R
@@ -3,22 +3,22 @@ context("Limma Analysis")
 test_that("limmaAnalysis finishes with result", {
   load(file = system.file("testdata/GSE27112-GPL6103.rda", package="phantasus"))
   expect_is(limmaAnalysis(es,
-                          fieldValues = c(rep("A", 3), rep("B", 2))), "json")
+                          fieldValues = c(rep("Target", 3), rep("Reference", 2))), "json")
 })
 
 test_that("limmaAnalysis works when there is only one phenotype attribute", {
     load(file = system.file("testdata/GSE27112-GPL6103.rda", package="phantasus"))
     pData(es) <- pData(es)[, "title", drop=F]
     expect_is(limmaAnalysis(es,
-                            fieldValues = c(rep("A", 3), rep("B", 2))), "json")
+                            fieldValues = c(rep("Target", 3), rep("Reference", 2))), "json")
 })
 
 
-test_that("limmaAnalysisImpl works", {
+test_that("limmaAnalysisSimpleImpl works", {
     load(file = system.file("testdata/GSE27112-GPL6103.rda", package="phantasus"))
-    de <- limmaAnalysisImpl(es, fieldValues = c(rep("A", 2), rep("B", 2), NA))
+    de <- limmaAnalysisSimpleImpl(es, fieldValues = c(rep("Target", 2), rep("Reference", 2), NA))
 
-    expect_equal(de$logFC[1], mean(exprs(es)[1, 3:4]) - mean(exprs(es)[1, 1:2]))
+    expect_equal(de$logFC[1], mean(exprs(es)[1, 1:2]) - mean(exprs(es)[1, 3:4]))
 })
 
 # Limma works for full dataset only
