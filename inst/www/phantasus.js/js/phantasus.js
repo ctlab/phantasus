445e6db3f2bbce0677a90cde21c90858947f7de1
Alexey Sergushichev <alsergbox@gmail.com>
2025-02-11 15:16:03 -0600

    fix saving to gct bug

--- inst/www/phantasus.js/js/phantasus.js
+++ inst/www/phantasus.js/js/phantasus.js
@@ -3664,7 +3664,7 @@ phantasus.GctWriter.prototype = {
         var vector = columnMetadata.get(columnMetadataIndex);
         var value = vector.getValue(j);
         if (value != null) {
-          toString = phantasus.VectorTrack.vectorToString(columnMetadata.get(j));
+          toString = phantasus.VectorTrack.vectorToString(columnMetadata.get(columnMetadataIndex));
           pw.push(toString(value));
         }
       }
